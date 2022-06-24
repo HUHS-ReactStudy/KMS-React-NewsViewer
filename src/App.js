717baf5c896f46789f395494f90e0d96160d493a
KIMMINSIK <we_in@naver.com>
2022-06-24 11:07:18 +0900

    remove debug code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { useState, useCallback } from "react";
-import axios from "axios";
-import NewsList from "./components/NewsList";
-import Categories from "./components/Categories";
 import { Route, Routes } from "react-router-dom";
 import NewsPage from "./components/Newspage";
 
